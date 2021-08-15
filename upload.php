e383fc63ea8c6b58b1a03306f2a9d378adf99fee
olivercrockett <87187168+olivercrockett@users.noreply.github.com>
2021-08-15 18:53:57 +0100

    Update upload.php

--- upload.php
+++ upload.php
@@ -1,11 +1,10 @@
-$host="localhost";
-$port=3306;
-$socket="";
-$user="root";
-$password="";
-$dbname="pi_webinars";
-
-$con = new mysqli($host, $user, $password, $dbname, $port, $socket)
-	or die ('Could not connect to the database server' . mysqli_connect_error());
-
-//$con->close();
+$target = "upload/";
+$target = $target . basename( $_FILES['uploaded']['name']) ;
+$ok=1;  if(move_uploaded_file($_FILES['uploaded']['tmp_name'], $target))
+{
+echo "The file ". basename( $_FILES['uploadedfile']['name']). " has been uploaded";
+}
+else {
+echo "Sorry, there was a problem uploading your file.";
+}
+?>

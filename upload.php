d88e486b93d2856b9149d21a7ddfe7b89a8fb15e
olivercrockett <87187168+olivercrockett@users.noreply.github.com>
2021-08-20 11:35:41 +0100

    Update upload.php

--- upload.php
+++ upload.php
@@ -1,10 +1,9 @@
-$target = "upload/";
-$target = $target . basename( $_FILES['uploaded']['name']) ;
-$ok=1;  if(move_uploaded_file($_FILES['uploaded']['tmp_name'], $target))
-{
-echo "The file ". basename( $_FILES['uploadedfile']['name']). " has been uploaded";
-}
-else {
-echo "Sorry, there was a problem uploading your file.";
-}
-?>
+<?php
+
+$connect = mysql_connect(“server_name”, “admin_name”, “password”); if (!connect) { die('Connection Failed: ' . mysql_error()); { mysql_select_db(“database_name”, $connect);
+                                                                                                                                
+$user_info = “INSERT INTO table_name (username, url) VALUES ('$_POST[username]', '$_POST[url]')”; if (!mysql_query($user_info, $connect)) { die('Error: ' . mysql_error()); }
+
+echo “Submitted”;
+
+mysql_close($connect); ?>

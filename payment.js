55fa98c3c3ce8dd85d7885a98bd1d18c36fb98aa
olivercrockett <87187168+olivercrockett@users.noreply.github.com>
2021-08-27 21:27:01 +0100

    Update payment.js

--- payment.js
+++ payment.js
@@ -1,8 +1,9 @@
 function rewardPi() {
   if date.getDate() = Date(x,x,1) {
-  document.getElementById(clicked_id) {
-    Pi.createPayment(amount: 'x*0.8*'${view_count}'', memo: 'PI REWARD FROM YOUR WEBINARS', metadata: { 0000 }, address: '${username}')
-  };
+  var x = document.getElementById(`${id}`) {
+    Pi.createPayment(amount: 'x*0.8*`${view_count}`', memo: 'PI REWARD FROM YOUR WEBINARS', metadata: { 0000 }, address: '${username}');
+  }
+  else //repeat next day
 }
 
 x = //one GBP worth of Pi

c60d514e75caa3e8475c4e389d34ffd8e35ac036
olivercrockett <87187168+olivercrockett@users.noreply.github.com>
2021-08-27 21:24:14 +0100

    Delete upload.php

--- upload.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-$connect = mysql_connect(“server_name”, “admin_name”, “password”); if (!connect) { die('Connection Failed: ' . mysql_error()); { mysql_select_db(“database_name”, $connect);
-                                                                                                                                
-$user_info = “INSERT INTO table_name (username, url) VALUES ('$_POST[username]', '$_POST[url]')”; if (!mysql_query($user_info, $connect)) { die('Error: ' . mysql_error()); }
-
-echo “Submitted”;
-
-mysql_close($connect); ?>

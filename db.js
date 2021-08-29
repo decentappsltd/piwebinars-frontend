14972f051646afc65d4f5e79e2bb909e9ca25af2
olivercrockett <87187168+olivercrockett@users.noreply.github.com>
2021-08-29 11:24:23 +0100

    Update db.js

--- db.js
+++ db.js
@@ -1,5 +1,5 @@
 const { MongoClient } = require('mongodb');
-const uri = "mongodb+srv://olivercrockett:<Ol1vermc>@cluster0.6eamu.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const uri = "mongodb+srv://olivercrockett:<Ol1vermc>@cluster0.6eamu.mongodb.net/Pi-Webinars?retryWrites=true&w=majority";
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 client.connect(err => {
   const collection = client.db("test").collection("devices");

0fb05dbacf9105a9128b80f8e71c93f67dd8734d
olivercrockett <87187168+olivercrockett@users.noreply.github.com>
2021-08-29 17:11:42 +0100

    Delete db.js

--- db.js
+++
@@ -1,8 +0,0 @@
-const { MongoClient } = require('mongodb');
-const uri = "mongodb+srv://olivercrockett:<Ol1vermc>@cluster0.6eamu.mongodb.net/Pi%20Webinars?retryWrites=true&w=majority";
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
-client.connect(err => {
-  const collection = client.db("test").collection("devices");
-  // perform actions on the collection object
-  client.close();
-});

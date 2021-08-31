e6c75424d5fba5c28cc1d1da7151c27dcf652041
olivercrockett <87187168+olivercrockett@users.noreply.github.com>
2021-08-31 13:13:51 +0100

    Delete payment.js

--- payment.js
+++
@@ -1,9 +0,0 @@
-function rewardPi() {
-  if date.getDate() = Date(x,x,1) {
-  var x = document.getElementById(`${id}`) {
-    Pi.createPayment(amount: 'x*0.8*`${view_count}`', memo: 'PI REWARD FROM YOUR WEBINARS', metadata: { 0000 }, address: '${username}');
-  }
-  else //repeat next day
-}
-
-x = //one GBP worth of Pi

c2103e31e5fb168c58d555830fa90c13d681936b
olivercrockett <87187168+olivercrockett@users.noreply.github.com>
2021-12-18 19:28:04 +0000

    Update pipayment.js

--- js/pipayment.js
+++ js/pipayment.js
@@ -29,103 +29,106 @@ $(document).ready(function () {
             });
         })
         .catch(err);
+    } catch (error) {
+      console.error(error);
     }
   }
 
-$(".button_click").click(function (webinarId, creatorId, categoryId) {
+$(".button_click").click(function(webinarId, creatorId, categoryId) {
   try {
-    const payment = Pi.createPayment(
-      {
+   Pi.createPayment({
         amount: 1,
         memo: "Webinar Purchase",
         metadata: { webinarId }
-      },
-      {
-        onReadyForServerApproval: function (paymentId) {
-          const paymentId = payment.identifier;
-          axios.post("/payment/approve", function(res, req) {
-            res.send(paymentId);
-          });
-        },
-        onReadyForServerCompletion: function (paymentId, txid) {
-          const paymentId = payment.identifier;
-          const txid = payment.transaction.txid;
-          const user_id = creatorId;
-          axios.post("/payment/complete", function(res, req) {
-            res.send(user_id, paymentId, txid);
-          });
-          showWebinar(webinarId, creatorId, categoryId);
-        }
-      }
-    );
-  } catch (err) {
-    alert(err);
+}, {
+  onReadyForServerApproval: function(paymentId) {           
+    axios.post("/payment/approve", function(res, req) {
+      res.send(paymentId);
+          }); 
+   },
+  onReadyForServerCompletion: function(paymentId, txid) {
+    const user_id = creatorId;
+    axios.post("/payment/complete", function(res, req) {
+      res.send(user_id, paymentId, txid);
+    });
+    showWebinar(webinarId, creatorId, categoryId);
+  },
+  onCancel: function(paymentId) { /* ... */ },
+  onError: function(error, payment) { /* ... */ },
+});
+  } catch (error) {
+    console.error(error);
   }
 });
 });
-  
-  const showWebinar = (webinarId, creatorId, categoryId) => {
-    var userId = creatorId;
-    var file_id = webinarId;
-    var collection_name = categoryId;
-    const purchasedWebinarJSON = axios.get(`/upload/${collection_name}/${userId}/${file_id}`);
-    const purchasedWebinar = JSON.parse(purchasedWebinarJSON);
-    const currentWebinar = purchasedWebinar['members'];
-    const modalId = this.dataset.open;
-    document.getElementById(modalId).classList.add(isVisible);
-    document.getElementById('usersPurchase').src = currentWebinar[i].file;
-    renderComments(purchasedWebinar);
-    const profileCurrentUser2 = axios.get('/profile');
-    const profileCurrentUser1 = JSON.parse(profileCurrentUser2);
-    const profileCurrentUser = obj['members'];
-    const userId1 = profileCurrentUser[i].userId;
-    axios.post(`/post/purchases/${userId1}/${collection_name}/${userId}/${file_id}`);
-  };
-                  ion renderComments(purchasedWebinar) {
-  const currentComments = purchasedWebinar['members'];
-  const commentDiv = document.getElementById('comments');
+
+const showWebinar = (webinarId, creatorId, categoryId) => {
+  var userId = creatorId;
+  var file_id = webinarId;
+  var collection_name = categoryId;
+  const purchasedWebinarJSON = axios.get(
+    `/upload/${collection_name}/${userId}/${file_id}`
+  );
+  const purchasedWebinar = JSON.parse(purchasedWebinarJSON);
+  const currentWebinar = purchasedWebinar["members"];
+  const modalId = this.dataset.open;
+  document.getElementById(modalId).classList.add(isVisible);
+  document.getElementById("usersPurchase").src = currentWebinar[i].file;
+  renderComments(purchasedWebinar);
+  const profileCurrentUser2 = axios.get("/profile");
+  const profileCurrentUser1 = JSON.parse(profileCurrentUser2);
+  const profileCurrentUser = obj["members"];
+  const userId1 = profileCurrentUser[i].userId;
+  axios.post(
+    `/post/purchases/${userId1}/${collection_name}/${userId}/${file_id}`
+  );
+};
+
+function renderComments(purchasedWebinar) {
+  const currentComments = purchasedWebinar["members"];
+  const commentDiv = document.getElementById("comments");
   for (let i = 0; i < currentComments.length; i++) {
-  const renderMessage = document.createElement('p3');
-  const renderCommentLikes = document.createElement('h6');
-  const renderLikeButton = document.createElement('button');
-  renderLikeButton.className = "likeComment";
-  renderLikeButton.onclick = function () {
+    const renderMessage = document.createElement("p3");
+    const renderCommentLikes = document.createElement("h6");
+    const renderLikeButton = document.createElement("button");
+    renderLikeButton.className = "likeComment";
+    renderLikeButton.onclick = function () {
       var webinarId = currentComments[i].post_id;
       var creatorId = currentComments[i].userId;
       var commentId = currentComments[i].comment_id;
-};
-  const renderReplyButton = document.createElement('button');
-  renderReplyButton.className = "replyComment";
-  renderReplyButton.onclick = function () {
-      var webinarId = currentComments[i].post_id;
+    };
+    const renderReplyButton = document.createElement("button");
+    renderReplyButton.className = "replyComment";
+    renderReplyButton.onclick = function () {
+      var webinarId = curetComments[i].post_id;
       var creatorId = currentComments[i].userId;
       var commentId = currentComments[i].comment_id;
-};
-  renderMessage.textContent = currentComments[i].comment_text;
-  renderCommentLikes.textContent = currentComments[i].comment_likes;
-  renderLikeButton.textContent = "Like Comment";
-  renderReplyButton.textContent = "Reply Comment";
-  
+    };
+    renderMessage.textContent = currentComments[i].comment_text;
+    renderCommentLikes.textContent = currentComments[i].comment_likes;
+    renderLikeButton.textContent = "Like Comment";
+    renderReplyButton.textContent = "Reply Comment";
+
     commentDiv.appendChild(renderMessage);
     commentDiv.appendChild(renderCommentLikes);
     commentDiv.appendChild(renderLikeButton);
     commentDiv.appendChild(renderReplyButton);
-    
-    section.appendChild(commentDiv);
-};
-};
 
-     function openCinema() {
-document.getElementById("modal1").classList.add("is-visible");
-};
+    section.appendChild(commentDiv);
+  }
+}
+
+function openCinema() {
+  document.getElementById("modal1").classList.add("is-visible");
+}
 
 function closeCinema() {
   document.getElementById("modal1").classList.remove("is-visible");
 }
 
-     function openUploadPage() {
-document.getElementById("modal2").classList.add("is-visible");
-};
+function openUploadPage() {
+  document.getElementById("modal2").classList.add("is-visible");
+}
 
 function closeUploadPage() {
   document.getElementById("modal2").classList.remove("is-visible");

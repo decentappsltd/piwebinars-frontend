23e4313188c34da71bde9889ea30930e4ebb6c23
olivercrockett <olivercrockett@crockettmotorcompany.com>
2022-02-22 18:24:46 +0000

    added Pi Payment

--- js/pipayment.js
+++ js/pipayment.js
@@ -1,26 +1,27 @@
+<<<<<<< Updated upstream
 const status = 'true';
 
+=======
+>>>>>>> Stashed changes
 async function auth() {
-    const scopes = ["username", "payments"];
-    function onIncompletePaymentFound(payment) {
-      var data = {
-        action: "complete",
-        paymentId: payment.identifier,
-        txid: payment.transaction.txid,
-      };
-      axios.post("https://server.piwebinars.co.uk/payment/cancel", function(res, req) {
-        res.send(data);
-      });
-    }
-    Pi.authenticate(scopes, onIncompletePaymentFound)
-      .then(async function (auth) {
-        const userName = auth.user.username;
-        document.getElementById("username").innerHTML = userName;
-      })
-    }
+  const scopes = ["username", "payments"];
+  function onIncompletePaymentFound(payment) {
+    var data = {
+      paymentId: payment.identifier,
+      txid: payment.transaction.txid,
+    };
+    axios.post("https://piwebinars-server.herokuapp.com/payment/incomplete", data);
+  }
+  Pi.authenticate(scopes, onIncompletePaymentFound)
+    .then(async function (auth) {
+      const userName = auth.user.username;
+      document.getElementById("username").innerHTML = userName;
+    })
+  }
 
 auth();
 
+<<<<<<< Updated upstream
 function buyWebinar(webinarId, creatorId) {
   if (status == 'true') {
     const creditAmountJSON = axios.get("https://server.piwebinars.co.uk/profile");
@@ -123,4 +124,45 @@ function testPayment(onReadyForServerApproval, onReadyForServerCompletion, onCan
     onError: onError
   };
   window.Pi.createPayment(paymentData, callbacks);
+=======
+function testPayment() {
+const payment = Pi.createPayment(
+{
+  amount: 1,
+  memo: "testing",
+  metadata: { paymentType: "testing" }
+},
+{
+  onReadyForServerApproval: function (paymentId) {
+    var data = {
+      paymentId: paymentId,
+      txid: "",
+    };
+    axios.post("https://piwebinars-server.herokuapp.com/payment/approve", data)
+  },
+  onReadyForServerCompletion: function (paymentId, txid) {
+    var data = {
+      paymentId: paymentId,
+      txid: txid,
+    };
+    axios.post("https://piwebinars-server.herokuapp.com/payment/complete", data)
+    // showWebinar();
+  },
+  onCancel: function (paymentId, txid) {
+    var data = {
+      paymentId: paymentId,
+      txid: txid,
+    };
+    axios.post("https://piwebinars-server.herokuapp.com/payment/incomplete", data);
+  },
+  onError: function (paymentId, txid) {
+    var data = {
+      paymentId: paymentId,
+      txid: txid,
+    };
+    axios.post("https://piwebinars-server.herokuapp.com/payment/incomplete", data);
+  }
+}
+);
+>>>>>>> Stashed changes
 }

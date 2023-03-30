24f1d187fdc8495eb3d32bcd33be67d0693f9481
olivercrockett <olivercrockett@crockettmotorcompany.com>
2023-03-30 12:03:36 +0100

    Update Posts.js

--- src/components/Posts.js
+++ src/components/Posts.js
@@ -177,7 +177,7 @@ function Posts(props) {
           }
 
           let ad = false;
-          if (index % 4 == 0) ad = true;
+          if (index == 0) ad = true;
           if (ad === true) setTimeout(pushAds, 3000);
 
           return (

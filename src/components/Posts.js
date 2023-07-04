82d7df9fc1e61056aebca8de378750831a392700
olivercrockett <olivercrockett1811@gmail.com>
2023-07-04 16:16:24 +0100

    Testing ads

--- src/components/Posts.js
+++ src/components/Posts.js
@@ -162,7 +162,11 @@ function Posts(props) {
               {
                 ad === true &&
                 <>
-                  <iframe data-aa='1980365' src='//ad.a-ads.com/1980365?size=300x250' style={{width: '300px', height: '250px', margin: '50px', border: '0px', padding: '0', overflow: 'hidden', backgroundColor: 'transparent'}}></iframe>
+                  {
+                    index !== 0 ?
+                      <iframe data-aa='1980365' src='//ad.a-ads.com/1980365?size=300x250' style={{ width: '300px', height: '250px', margin: '50px', border: '0px', padding: '0', overflow: 'hidden', backgroundColor: 'transparent' }}></iframe> :
+                      <a href='https://apitopackage.zip'><img  src='https://apitopackage.zip/ad.png' style={{ width: '300px', height: '250px', margin: '50px', border: '0px', padding: '0', overflow: 'hidden', backgroundColor: 'transparent' }}></img></a>
+                  }
                 </>
               }
             </>
